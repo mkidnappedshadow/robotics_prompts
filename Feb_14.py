af4bc386dc3f36a7993db7fd9196fc989b30e4fe
mkidnappedshadow <mohamedm192@stemcollegiate.ca>
2025-02-14 09:31:56 -0700

    Create Feb_14.py

---
+++ Feb_14.py
@@ -0,0 +1,5 @@
+print ("Mohamed")
+print ("Febuary 14th 2025")
+print (" I am singing up for the replit's 100 days of phython challenge! I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!")
+print ("I am feeling 😛")
+print ("You can follow my progress at replit.com/@mohamedm192")
